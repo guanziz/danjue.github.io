862fc27d5588edb22ca88841de54ec1754dab098
guan'zi'z <1926718909@qq.com>
2023-06-18 17:12:31 +0800

    1
    1

---
+++ js/iframe.js
@@ -0,0 +1,5 @@
+function adjustIframe(){
+    var ifm= document.getElementById("bi_iframe");
+    ifm.height=document.documentElement.clientHeight;
+    ifm.width=document.documentElement.clientWidth;
+}
